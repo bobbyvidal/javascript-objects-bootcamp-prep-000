3d7b060202768fa65a450ec4b344fcff552d4533
Robert Vidal <bobbyvidal@gmail.com>
2018-10-02 15:20:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
-var playlist = {}
-function updatePlaylist(playlist, artistName, songTitle) {
-playlist.artist3 = 'song3';
-  return playlist;
+
+var playlist = new Object({keys: '0'});
+ function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+ function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
 }
